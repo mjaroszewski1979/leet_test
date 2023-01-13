e8459d1c77e622e53aeafd61ce9e0b7b34cd9255
mjaroszewski <mjaroszewski1979@gmail.com>
2023-01-13 22:30:12 +0100

    created email_content file to enable program refactoring

---
+++ email_content.py
@@ -0,0 +1,28 @@
+def get_content(cmp_name):
+
+    content = '''
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <div style="padding:10px 20px;text-align:justify;font-family:Georgia, 'Times New Roman', Times, serif;">
+                    <p>Dear Sirs,</p>
+                    <br>
+                    <br>
+                    <p>I am writing to express my interest in the software development opportunities at {}. I recently came across your company and was impressed by the innovative products and technologies you are working on.
+                        Although I do not have a computer science degree, I have always been interested in technology and have spent the past few years teaching myself how to code in multiple programming languages, including AFL, MQL4 and Python.</p>
+                    <br>
+                    <p>I have completed a number of online courses and projects, including building a web application and developing algorithmic trading strategies. I am eager to start my career in software development and believe that {} would be a great place for me to grow and learn. I am confident that my enthusiasm for technology and strong work ethic make me a valuable asset to your team.
+                        I am attaching my resume for your review. I would be grateful for the opportunity to discuss my qualifications further and learn more about the opportunities at {}. Thank you for considering my application.</p>
+                    <br>
+                    <p>Sincerely,</p>
+                    <br>
+                    <br>
+                    <h5>Maciej Jaroszewski</h5>
+                    <p><span style="font-size:smaller;color:grey;">mjaroszewski.website</span><p/>
+                    <p><span style="font-size:smaller;color:grey;">github.com/mjaroszewski1979</span><p/>
+                </div>
+            </body>
+        </html>
+        '''.format(cmp_name, cmp_name, cmp_name)
+
+    return content
--- send_email.py
+++ send_email.py
@@ -2,11 +2,15 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 from pdf_sender import get_pdf
+from email_content import get_content
+
 import time
 
 def send_email(company_name, company_email):
     get_pdf(company=company_name)
     time.sleep(5)
+    content = get_content(company_name)
+
 
     smtp_port = 587
     smtp_server = 'smtp.gmail.com'
@@ -20,30 +24,7 @@ def send_email(company_name, company_email):
     msg['Subject'] = 'Software Developer'
     msg['From'] = email_from
     msg['To'] = email_to
-    msg.set_content('''
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <div style="padding:10px 20px;text-align:justify;font-family:Georgia, 'Times New Roman', Times, serif;">
-                <p>Dear Sirs,</p>
-                <br>
-                <br>
-                <p>I am writing to express my interest in the software development opportunities at {}. I recently came across your company and was impressed by the innovative products and technologies you are working on.
-                    Although I do not have a computer science degree, I have always been interested in technology and have spent the past few years teaching myself how to code in multiple programming languages, including AFL, MQL4 and Python.</p>
-                <br>
-                <p>I have completed a number of online courses and projects, including building a web application and developing algorithmic trading strategies. I am eager to start my career in software development and believe that {} would be a great place for me to grow and learn. I am confident that my enthusiasm for technology and strong work ethic make me a valuable asset to your team.
-                    I am attaching my resume for your review. I would be grateful for the opportunity to discuss my qualifications further and learn more about the opportunities at {}. Thank you for considering my application.</p>
-                <br>
-                <p>Sincerely,</p>
-                <br>
-                <br>
-                <h5>Maciej Jaroszewski</h5>
-                <p><span style="font-size:smaller;color:grey;">mjaroszewski.website</span><p/>
-                <p><span style="font-size:smaller;color:grey;">github.com/mjaroszewski1979</span><p/>
-            </div>
-        </body>
-    </html>
-    '''.format(company_name, company_name, company_name), subtype='html')
+    msg.set_content(content, subtype='html')
 
     pdf_files = ['Maciej_Jaroszewski_CL.pdf', 'Maciej_Jaroszewski_CV.pdf']
 
