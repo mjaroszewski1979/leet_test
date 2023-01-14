0da404df8dd188360a3ecdb98f1556d0e467406a
mjaroszewski <mjaroszewski1979@gmail.com>
2023-01-14 22:36:35 +0100

    change string formatting method to make code more dynamic

--- email_content.py
+++ email_content.py
@@ -1,6 +1,6 @@
 def get_content(cmp_name):
 
-    content = '''
+    content = f'''
         <!DOCTYPE html>
         <html>
             <body>
@@ -8,11 +8,11 @@ def get_content(cmp_name):
                     <p>Dear Sirs,</p>
                     <br>
                     <br>
-                    <p>I am writing to express my interest in the software development opportunities at {}. I recently came across your company and was impressed by the innovative products and technologies you are working on.
+                    <p>I am writing to express my interest in the software development opportunities at {cmp_name}. I recently came across your company and was impressed by the innovative products and technologies you are working on.
                         Although I do not have a computer science degree, I have always been interested in technology and have spent the past few years teaching myself how to code in multiple programming languages, including AFL, MQL4 and Python.</p>
                     <br>
-                    <p>I have completed a number of online courses and projects, including building a web application and developing algorithmic trading strategies. I am eager to start my career in software development and believe that {} would be a great place for me to grow and learn. I am confident that my enthusiasm for technology and strong work ethic make me a valuable asset to your team.
-                        I am attaching my resume for your review. I would be grateful for the opportunity to discuss my qualifications further and learn more about the opportunities at {}. Thank you for considering my application.</p>
+                    <p>I have completed a number of online courses and projects, including building a web application and developing algorithmic trading strategies. I am eager to start my career in software development and believe that {cmp_name} would be a great place for me to grow and learn. I am confident that my enthusiasm for technology and strong work ethic make me a valuable asset to your team.
+                        I am attaching my resume for your review. I would be grateful for the opportunity to discuss my qualifications further and learn more about the opportunities at {cmp_name}. Thank you for considering my application.</p>
                     <br>
                     <p>Sincerely,</p>
                     <br>
@@ -23,6 +23,6 @@ def get_content(cmp_name):
                 </div>
             </body>
         </html>
-        '''.format(cmp_name, cmp_name, cmp_name)
+        '''
 
     return content
